d775833b0a952bfbd7864c64f39a31b9f8fc8aa1
Vytautas Bielinskas <vytautas.bielinskas@gmail.com>
2021-03-24 14:11:08 +0200

    Update model.py

--- model.py
+++ model.py
@@ -84,7 +84,7 @@ model.compile(loss = tf.keras.losses.mae,
               metrics = ['mae'])
 
 # Fit the model
-model.fit(X_train, y_train, epochs=100)
+model.fit(X_train, y_train, epochs=50)
 
 
 # Make and plot predictions for model_1
